36e1af91e737d483881e5179b14779e295f67f6d
ritwik bhagat <ritwikbhagat2025@gmail.com>
2026-01-30 03:11:56 +0530

    initial changes

---
+++ section 9/accounts/src/main/java/com/eazybtes/accounts/service/ICustomersService.java
@@ -0,0 +1,11 @@
+package com.eazybtes.accounts.service;
+import com.eazybtes.accounts.dto.CustomerDetailsDto;
+
+public interface ICustomersService {
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
+}
